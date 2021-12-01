714ff12ceb56e17cc6c287f2efee7652656fcc8f
Omkar Pawar <opawar88@gmail.com>
2021-12-01 09:56:05 +0530

    Uploade ClassLibrary And Maths Project

---
+++ Class Library Assignment/CalculatorFn/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace CalculatorFn
+{
+    public class FnClassLibrary
+    {
+        public int DoSum(int firstNum, int secondNum)
+        {
+            return firstNum + secondNum;
+        }
+
+        public int DoSub(int firstNum, int secondNum)
+        {
+            return firstNum - secondNum;
+        }
+
+        public int DoMulti(int firstNum, int secondNum)
+        {
+            return firstNum * secondNum;
+        }
+
+        public int DoDivi(int firstNum, int secondNum)
+        {
+            return firstNum / secondNum;
+        }
+    }
+}
