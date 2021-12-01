0b22ad2654e8cba88d9a5a32857057f8696a5495
Omkar Pawar <opawar88@gmail.com>
2021-12-01 10:16:45 +0530

    Working Model Class Library

---
+++ Class Library Assignment/C_ClassLibrary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CalculatorFn;
+
+namespace C_ClassLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 1 No : ");
+            int no1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter 2 No : ");
+            int no2 = Int32.Parse(Console.ReadLine());
+            FnClassLibrary fnClassLibrary = new FnClassLibrary();
+            Console.WriteLine("Sum : {0} + {1} = {2}",no1,no2,fnClassLibrary.DoSum(no1,no2));
+            Console.WriteLine("Sub : {0} - {1} = {2}",no1,no2,fnClassLibrary.DoSub(no1,no2));
+            Console.WriteLine("Multi : {0} * {1} = {2}",no1,no2,fnClassLibrary.DoMulti(no1,no2));
+            Console.WriteLine("Division : {0} / {1} = {2}",no1,no2,fnClassLibrary.DoDivi(no1,no2));
+            Console.Read();
+        }
+    }
+}
